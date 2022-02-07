f968026bd38921e6857a949df436dc0eb7f1bb28
Archit Jain <arcyjain2002@gmail.com>
2022-02-08 02:15:00 +0530

    testing for updation

--- src/backend/controllers/postController.js
+++ src/backend/controllers/postController.js
@@ -1,3 +1,5 @@
+const express = require("express");
+const { response } = require("express");
 const mongoose = require("mongoose");
 var PostModel = require("../models/postModel.js");
 
@@ -13,12 +15,48 @@ exports.findPostDetails = function (req, res) {
 };
 exports.insertPostDetails = function (req, res) {
   const postObj = new PostModel(req.body);
-  postObj.save((err, data) => {
+  console.log(postObj);
+  PostModel.findOne({ InstId: postObj.InstId }, (err, data) => {
     if (err) {
       res.sendStatus(400);
     } else {
-      console.log(data);
-      res.sendStatus(200);
+      if (data == null) {
+        postObj.save();
+        res.send("new data inserted");
+      } else {
+        // data = JSON.parse(data);
+        // data.PostCount = PostCount + 1;
+        PostModel.updateMany(
+          { InstId: postObj.InstId },
+          {
+            $push: {
+              InstPosts: {
+                PostDescription: req.body.PostDescription,
+                PostPhotoUrl: req.body.PostPhotoUrl,
+                Location: req.body.Location,
+                LikesCount: req.body.LikesCount,
+                CommentCount: req.body.CommentCount,
+                PostDate: req.body.PostDate,
+                Comments: [
+                  {
+                    InstId: req.body.InstId,
+                    CommentText: req.body.CommentText,
+                    CommentToId: req.body.CommentToId,
+                  },
+                ],
+              },
+            },
+          }
+        )
+          .then((data) => {
+            console.log(data);
+            res.sendStatus(200);
+          })
+          .catch((err) => {
+            console.log(err);
+            res.sendStatus(400);
+          });
+      }
     }
   });
 };
